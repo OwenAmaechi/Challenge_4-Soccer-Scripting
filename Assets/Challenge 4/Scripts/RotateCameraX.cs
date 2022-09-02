8c36180848e4d2146d62fbae08fdc854eb3af621
Owen_A <chinazo.owen@gmail.com>
2022-09-02 04:25:59 +0100

    Initial commit

---
+++ Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraX : MonoBehaviour
+{
+  private float speed = 200;
+  public GameObject player;
+  public ParticleSystem turboSmoke;
+
+  // Update is called once per frame
+  void Update()
+  {
+    float horizontalInput = Input.GetAxis("Horizontal");
+    transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+    transform.position = player.transform.position; // Move focal point with player
+    if (Input.GetKeyDown(KeyCode.Space))
+    { turboSmoke.Play(); }
+
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Challenge_4-Soccer-Scripting
+ soccer scripting challenge
